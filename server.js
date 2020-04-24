67a613fa4f0db9c9df4b44889fe6fb04b7d09694
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-04-24 23:58:25 +0800

    Change fallback port number

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
         });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold);
